bd6789243dd15af454710858cef46ae726576e95
oakes680 <chris.oakes@outlook.com>
2020-02-05 21:41:33 -0800

    added swipe

--- front-end/src/jobSeeker/components/JobSeekerSwipe.js
+++ front-end/src/jobSeeker/components/JobSeekerSwipe.js
@@ -1,6 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { axiosWithAuthJobSeeker } from '../utils/axiosWithAuthJobSeeker';
+import { useSwipeable } from 'react-swipeable';
+import {
+	HomeStyled,
+	StyledHeading,
+	Form,
+	Input,
+	Links,
+	LinkStyled,
+	ButtonStyled,
+	PurpleText,
+	Highlighted,
+	HoverText,
+	EditDisplayFlex,
+	DescriptionStyled,
+	StyledDashboardHeading,
+	DashboardButton,
+	DashboardCenter,
+	EditButtonDisplayFlex,
+	EditScreenText,
+	CardDiv
+} from '../../styledcomp/Home';
 
 const JobSeekerSwipe = () => {
 	const id = localStorage.getItem('jobseekerid');
@@ -48,17 +69,38 @@ const JobSeekerSwipe = () => {
 	const nofunction = () => {
 		//call random
 		randomfunction();
+		console.log('nooooooooooo')
 	};
 
-	if (state.companyAPIReducer.data[number].company == null) {
-		return <h1>no more jobs</h1>;
+	// const handlersWorking = useSwipeable({
+	// 	// onSwipedLeft: () => setTimeout(next, 0),
+	// 	onSwipedLeft: () => setTimeout(nofunction, 0),
+	// 	trackMouse: true,
+	// })
+
+	const handlersBugged = useSwipeable({
+		onSwipedLeft: nofunction,
+		trackMouse: true,
+	})
+
+
+	if (typeof state.companyAPIReducer.data[number] === 'undefined' || state.companyAPIReducer.data[number] === null) {
+		return (
+			<HomeStyled>
+				<StyledHeading>No more jobs, </StyledHeading>
+				<StyledHeading>time to go back to Tinder</StyledHeading>
+		</HomeStyled>)
+
 	} else {
 		return (
-			<div>
-				<h1>{state.companyAPIReducer.data[number].company}</h1>
-				<button onClick={yesfunction}>Yes</button>
-				<button onClick={nofunction}>No</button>
-			</div>
+			<CardDiv {...handlersBugged}>
+				<StyledHeading><p>	Company:</p>   {state.companyAPIReducer.data[number].company}</StyledHeading>
+				<StyledHeading>Salary: {state.companyAPIReducer.data[number].salary}</StyledHeading>
+				<StyledHeading>Job: {state.companyAPIReducer.data[number].job_position}</StyledHeading>
+				<StyledHeading>Educ: {state.companyAPIReducer.data[number].education}</StyledHeading>
+				{/* <DashboardButton onClick={yesfunction}>Yes</DashboardButton>
+				<DashboardButton onClick={nofunction}>No</DashboardButton> */}
+			</CardDiv>
 		);
 	}
 };
