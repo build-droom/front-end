1c31d49ace4f000195d40eb87426dd8e025c6560
oakes680 <chris.oakes@outlook.com>
2020-02-06 17:01:25 -0800

    styling

--- front-end/src/jobSeeker/components/JobSeekerSwipe.js
+++ front-end/src/jobSeeker/components/JobSeekerSwipe.js
@@ -5,177 +5,205 @@ import { useSwipeable } from 'react-swipeable';
 
 import { Card, CardWrapper } from 'react-swipeable-cards';
 
-
 import {
-	HomeStyled,
-	StyledHeading,
-	StyledHeadingSwipe,
-	Button,
-	HoverText,
-	EditDisplayFlex,
-	DescriptionStyled,
-	StyledDashboardHeading,
-	DashboardButton,
-	DashboardCenter,
-	EditButtonDisplayFlex,
-	EditScreenText,
-	CardDiv
+  HomeStyled,
+  StyledHeading,
+  StyledHeadingSwipe,
+  Button,
+  HoverText,
+  EditDisplayFlex,
+  DescriptionStyled,
+  StyledDashboardHeading,
+  DashboardButton,
+  DashboardCenter,
+  EditButtonDisplayFlex,
+  EditScreenText,
+  CardDiv
 } from '../../styledcomp/Home';
 
 const JobSeekerSwipe = props => {
-	const id = localStorage.getItem('jobseekerid');
-	console.log(id);
+  const id = localStorage.getItem('jobseekerid');
+  console.log(id);
 
-	const state = useSelector(state => state);
+  const state = useSelector(state => state);
 
-	console.log('this is state from swipe', state.companyAPIReducer.data);
+  console.log('this is state from swipe', state.companyAPIReducer.data);
 
-	//  Math.floor(Math.random() * Math.floor(3));
-	// const [save, setSave] = useState({
-	// 	seekers_id: id,
-	// 	joblisting_id: '4',
-	// 	fave_of_seeker: 'true',
-	// 	fave_for_job: 'false',
-	// 	matched_occupation: 'true'
-	// });
-	const [number, setNumber] = useState(0);
+  //  Math.floor(Math.random() * Math.floor(3));
+  // const [save, setSave] = useState({
+  // 	seekers_id: id,
+  // 	joblisting_id: '4',
+  // 	fave_of_seeker: 'true',
+  // 	fave_for_job: 'false',
+  // 	matched_occupation: 'true'
+  // });
+  const [number, setNumber] = useState(0);
 
-	useEffect(() => {}, [number]);
+  useEffect(() => {}, [number]);
 
-	const randomfunction = () => {
-		console.log('numnbenrbamndbksabdksja');
-		console.log(number);
-		setNumber(number + 1);
-	};
+  const randomfunction = () => {
+    console.log('numnbenrbamndbksabdksja');
+    console.log(number);
+    setNumber(number + 1);
+  };
 
-	const yesfunction = () => {
-		//post to matches
-		axiosWithAuthJobSeeker()
-			.post('/matches', {
-				seekers_id: id,
-				fave_of_seeker: 'true',
-				fave_for_job: 'false',
-				matched_occupation: 'true'
-			})
-			.then(res => {
-				//call random
-				randomfunction();
-			})
-			.catch();
-		//call random
-	};
+  const yesfunction = () => {
+    //post to matches
+    axiosWithAuthJobSeeker()
+      .post('/matches', {
+        seekers_id: id,
+        fave_of_seeker: 'true',
+        fave_for_job: 'false',
+        matched_occupation: 'true'
+      })
+      .then(res => {
+        //call random
+        randomfunction();
+      })
+      .catch();
+    //call random
+  };
 
+  const hello = () => {
+    console.log('hello');
+  };
 
-	const hello = () => {
-		console.log('hello')
-	}
+  const nofunction = () => {
+    //call random
+    randomfunction();
+    console.log('nooooooooooo');
+  };
 
-	const nofunction = () => {
-		//call random
-		randomfunction();
-		console.log('nooooooooooo');
-	};
+  // const handlersWorking = useSwipeable({
+  // 	// onSwipedLeft: () => setTimeout(next, 0),
+  // 	onSwipedLeft: () => setTimeout(nofunction, 0),
+  // 	trackMouse: true,
+  // })
 
-	// const handlersWorking = useSwipeable({
-	// 	// onSwipedLeft: () => setTimeout(next, 0),
-	// 	onSwipedLeft: () => setTimeout(nofunction, 0),
-	// 	trackMouse: true,
-	// })
+  const handlersBugged = useSwipeable({
+    onSwipedLeft: nofunction,
+    onSwipedRight: nofunction,
+    trackMouse: true
+  });
 
-	const handlersBugged = useSwipeable({
-		onSwipedLeft: nofunction,
-		onSwipedRight: nofunction,
-		trackMouse: true
-	});
+  const cardWrap = {
+    // display: 'flex',
+    // justifyContent: 'center',
+		 //border: "1px red solid",
+		 //paddingTop: '-1000px',
+		 height: '500px'
+		 
 
-	const cardWrap = {
-		display: "flex",
-		justifyContent: "center",
-		// border: "1px red solid"
-	}
+  };
 
-	const cardStyle = {
-		border: "1px #E5E5E5  solid",
-		textAlign: "center",
-		height: "70%",
-		marginTop: "-50px",
-		display: "flex",
-		flexDirection: 'column',
-		justifyContent: "center",
-		bordeRadius: '20px',
-		fontSize: '40px',
-	boxShadow: '0px 10px 30px 0px rgba(0, 0, 0, 0.1)',
-	transition: 'opacity 0.2s ease, box-shadow 0.2s ease',
-	opacity: '1'
+  const cardStyle = {
+    border: '1px #E5E5E5  solid',
+    textAlign: 'left',
+		height: '70%',
+		width: '80%',
+		marginTop: '100px;',
+		paddingTop: '40px',
+		paddingLeft: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+		//justifyContent: 'center',
+		// padding: '40px',
+    bordeRadius: '30px',
+		fontSize: '30px',
+		boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.1)',
+		//boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+    transition: 'opacity 0.2s ease, box-shadow 0.2s ease',
+		opacity: '1',
+		//pointerEvents: 'none'
 		
-	}
+  };
 
+  if (
+    typeof state.companyAPIReducer.data[number] === 'undefined' ||
+    state.companyAPIReducer.data[number] === null
+  ) {
+    return (
+      <>
+        <Button onClick={() => props.history.goBack()}>Back</Button>
+        <HomeStyled>
+          <Button onClick={() => props.history.goBack()}>Back</Button>
 
+          <StyledHeading>No more jobs, </StyledHeading>
+          <StyledHeading>time to go back to Tinder</StyledHeading>
+        </HomeStyled>
+      </>
+    );
+  } else {
+    return (
+      // <div>
+      // 	 <Button onClick={() => props.history.goBack()}>Back</Button>
 
-	if (
-		typeof state.companyAPIReducer.data[number] === 'undefined' ||
-		state.companyAPIReducer.data[number] === null
-	) {
-		return (
-			<HomeStyled>
-				<Button onClick={() => props.history.goBack()}>Back</Button>
+      // 		<CardDiv {...handlersBugged}>
+      // 		<StyledHeading>
+      // 			<p> Company:</p> {state.companyAPIReducer.data[number].company}
+      // 		</StyledHeading>
+      // 		<StyledHeading>
+      // 			Salary: {state.companyAPIReducer.data[number].salary}
+      // 		</StyledHeading>
+      // 		<StyledHeading>
+      // 			Job: {state.companyAPIReducer.data[number].job_position}
+      // 		</StyledHeading>
+      // 		<StyledHeading>
+      // 			Educ: {state.companyAPIReducer.data[number].education}
+      // 		</StyledHeading>
+      // 		<DashboardButton onClick={yesfunction}>Yes</DashboardButton>
+      // 		<DashboardButton onClick={nofunction}>No</DashboardButton>
 
-				<StyledHeading>No more jobs, </StyledHeading>
-				<StyledHeading>time to go back to Tinder</StyledHeading>
-			</HomeStyled>
-		);
-	} else {
-		return (
-			// <div>
-			// 	 <Button onClick={() => props.history.goBack()}>Back</Button>
-				
-			
-					
-			// 		<CardDiv {...handlersBugged}>
-			// 		<StyledHeading>
-			// 			<p> Company:</p> {state.companyAPIReducer.data[number].company}
-			// 		</StyledHeading>
-			// 		<StyledHeading>
-			// 			Salary: {state.companyAPIReducer.data[number].salary}
-			// 		</StyledHeading>
-			// 		<StyledHeading>
-			// 			Job: {state.companyAPIReducer.data[number].job_position}
-			// 		</StyledHeading>
-			// 		<StyledHeading>
-			// 			Educ: {state.companyAPIReducer.data[number].education}
-			// 		</StyledHeading>
-			// 		<DashboardButton onClick={yesfunction}>Yes</DashboardButton>
-			// 		<DashboardButton onClick={nofunction}>No</DashboardButton>
-					
-			// 	</CardDiv> 
-					
-			
+      // 	</CardDiv>
 
-			
-			// </div>
-			<>
-				<HomeStyled>
-			<StyledHeadingSwipe>Swipe Right to Find your Dream Job</StyledHeadingSwipe>
-				<CardWrapper style={cardWrap}>
-					{state.companyAPIReducer.data.map(item => {
-						return (
-							
-							<Card key={item} style={cardStyle} onSwipeRight={hello} >
-									<p>{item.salary}</p>
-									<p>{item.education}</p>
-									<p>{item.employment_type}</p>
-									<p>{item.job_location}</p>
-								</Card>
-							
-						)
-					})}
-				</CardWrapper>
-				</HomeStyled>
-			</>
-		
-			
-		);
-	}
+      // </div>
+      <>
+        <Button onClick={() => props.history.goBack()}>Back</Button>
+        <HomeStyled>
+          <StyledHeadingSwipe>
+            Swipe Right to Find your Dream Job
+          </StyledHeadingSwipe>
+          <CardWrapper style={cardWrap}>
+            {state.companyAPIReducer.data.map(item => {
+              return (
+                <Card key={item} style={cardStyle} onSwipeRight={hello}>
+                  <div>
+                    <label className="pLabel">Salary: </label>
+                    <span>{item.salary}</span>
+                  </div>
+
+                  <div>
+                    <label className="pLabel">Job: </label>
+                    <span>{item.job_position}</span>
+                  </div>
+
+                  <div>
+                    <label className="pLabel">Type: </label>
+                    <span>{item.employment_type}</span>
+                  </div>
+
+                  <div>
+                    <label className="pLabel">Education: </label>
+                    <span>{item.education}</span>
+                  </div>
+
+                  <div>
+                    <label className="pLabel">Skills: </label>
+                    <span>{item.skills}</span>
+                  </div>
+
+                  <div>
+                    <label className="pLabel">Location: </label>
+                    <span>{item.job_location}</span>
+                  </div>
+                </Card>
+              );
+            })}
+          </CardWrapper>
+        </HomeStyled>
+      </>
+    );
+  }
 };
 
 export default JobSeekerSwipe;
