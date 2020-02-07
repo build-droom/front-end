356768d8c940ae2a3a2d3561c56a7e96bd5a1c8c
kika <anamonteiro430@gmail.com>
2020-02-07 00:26:49 +0000

    showing the matches

--- front-end/src/jobSeeker/components/JobSeekerMatches.js
+++ front-end/src/jobSeeker/components/JobSeekerMatches.js
@@ -2,6 +2,19 @@ import React, { useEffect } from 'react';
 import { Button } from './../../styledcomp/Home';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMatches } from './../actions/jobSeekerAPIAction';
+import {
+	HomeStyled,
+	StyledHeading,
+	Form,
+	Input,
+	Links,
+	LinkStyled,
+	ButtonStyled,
+	PurpleText,
+	StyledDashboardHeading,
+	DashboardButton,
+	CardDiv2
+} from './../../styledcomp/Home';
 
 const JobSeekerMatches = props => {
 	const id = localStorage.getItem('jobseekerid');
@@ -13,12 +26,32 @@ const JobSeekerMatches = props => {
 		dispatch(getMatches(id));
 	}, []);
 
-	return (
-		<div>
-			<Button onClick={() => props.history.goBack()}>Back</Button>
-			<h1>This is the Job seeker matches</h1>
-		</div>
-	);
+	if (Array.isArray(state.matches) && state.matches.length === 0) {
+		return (
+			<>
+				<Button onClick={() => props.history.goBack()}>Back</Button>
+				<HomeStyled>
+					<StyledHeading>You have no matches</StyledHeading>
+				</HomeStyled>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<Button onClick={() => props.history.goBack()}>Back</Button>
+				<div>
+					<h1>This is the Job seeker matches</h1>
+					{state.matches.map((job, index) => (
+						<CardDiv2 className='jobs' key={index}>
+							<p>{job.job_position}</p>
+							<p>{job.company_name}</p>
+							<p>{job.job_location}</p>
+							<p>{job.salary}$/yr</p>
+						</CardDiv2>
+					))}
+				</div>
+			</>
+		);
+	}
 };
-
 export default JobSeekerMatches;
