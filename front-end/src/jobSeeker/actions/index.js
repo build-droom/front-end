c4d3bdf80e41ae900b7ea2394a7bd30d7a2fc3b1
kika <anamonteiro430@gmail.com>
2020-02-07 00:05:55 +0000

    matches working

--- front-end/src/jobSeeker/actions/index.js
+++ front-end/src/jobSeeker/actions/index.js
@@ -7,12 +7,10 @@ export { FETCH_DATA_START } from './jobSeekerAPIAction';
 export { FETCH_DATA_SUCCESS } from './jobSeekerAPIAction';
 export { FETCH_DATA_FAILURE } from './jobSeekerAPIAction';
 
-export { PUT_DATA_START } from './jobSeekerAPIAction';
-export { PUT_DATA_SUCCESS } from './jobSeekerAPIAction';
-export { PUT_DATA_FAILURE } from './jobSeekerAPIAction';
 export { PUSH_DATA_START } from './jobSeekerAPIAction';
 export { PUSH_DATA_SUCCESS } from './jobSeekerAPIAction';
 export { PUSH_DATA_FAILURE } from './jobSeekerAPIAction';
-export { DELETE_DATA_START } from './jobSeekerAPIAction';
-export { DELETE_DATA_SUCCESS } from './jobSeekerAPIAction';
-export { DELETE_DATA_FAILURE } from './jobSeekerAPIAction';
+
+export { FETCH_MATCHES_START } from './jobSeekerAPIAction';
+export { FETCH_MATCHES_SUCCESS } from './jobSeekerAPIAction';
+export { FETCH_MATCHES_FAILURE } from './jobSeekerAPIAction';
--- front-end/src/jobSeeker/components/JobSeekerMatches.js
+++ front-end/src/jobSeeker/components/JobSeekerMatches.js
@@ -1,22 +1,22 @@
 import React, { useEffect } from 'react';
-import { axiosWithAuthJobSeeker } from './../utils/axiosWithAuthJobSeeker';
 import { Button } from './../../styledcomp/Home';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMatches } from './../actions/jobSeekerAPIAction';
 
 const JobSeekerMatches = props => {
 	const id = localStorage.getItem('jobseekerid');
+	const dispatch = useDispatch();
+	const state = useSelector(state => state.jobSeekerAPIReducer);
+	console.log('this is state', state);
+
 	useEffect(() => {
-		axiosWithAuthJobSeeker()
-			.get(`/matches/matchseeker/${id}`)
-			.then(res => {
-				console.log('matches', res);
-			})
-			.catch(err => console.log(err));
+		dispatch(getMatches(id));
 	}, []);
 
 	return (
 		<div>
 			<Button onClick={() => props.history.goBack()}>Back</Button>
-			<h1>This is the Job seeker list</h1>
+			<h1>This is the Job seeker matches</h1>
 		</div>
 	);
 };
